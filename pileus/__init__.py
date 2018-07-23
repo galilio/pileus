0b40f693fb11673cf97bfa92044eb8e13168f85c
xiaoym <514058722@qq.com>
2018-07-23 20:51:09 +0800

    export run.

--- pileus/__init__.py
+++ pileus/__init__.py
@@ -1,6 +1,7 @@
 #-*- encoding: utf-8
 import bottle
 from bottle import route, get, post, delete, put, response, abort, request, Bottle
+from bottle import run
 from bottle import HTTPError
 from .async import async
 
@@ -12,4 +13,4 @@ request_method = ['get', 'post', 'delete', 'put', 'response', 'abort', 'request'
 def bp():
     return Bottle()
 
-__all__ = ['async', 'route', 'app', 'bp'] + request_method
+__all__ = ['async', 'route', 'app', 'run', 'bp'] + request_method
