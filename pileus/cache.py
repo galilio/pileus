474cbf52be41dcb6a5cf629527406b85d2fea905
xiaoym <514058722@qq.com>
2018-08-02 12:44:16 +0800

    load redis by inspect.

--- pileus/cache.py
+++ pileus/cache.py
@@ -1,6 +1,8 @@
 from nameko.extensions import DependencyProvider
 from functools import wraps
 from nameko.rpc import Rpc
+from redis.client import StrictRedis
+import inspect
 
 import pickle
 
@@ -15,6 +17,9 @@ def __generate_redis_key(keys, *args, **kwargs):
 
     return parts
 
+def __is_redis(obj):
+    return isinstance(obj, StrictRedis)
+
 def cache(prefix = 'bayesba', keys = [], timeout = 10 * 60):
     def decorate(func):
         @wraps(func)
@@ -22,24 +27,22 @@ def cache(prefix = 'bayesba', keys = [], timeout = 10 * 60):
             if len(args) < 1:
                 raise Exception('只能用在nameko service的方法上')
 
-            if not hasattr(args[0], 'redis'):
+            redis = None
+            for _, member in inspect.getmembers(args[0], __is_redis):
+                redis = member
+
+            if not redis:
                 raise Exception('需要引入redis依赖')
 
-            redis = args[0].redis
-
             parts = __generate_redis_key(keys, *args, **kwargs)
-
             parts.insert(0, func.__name__)
-
             if prefix:
                 parts.insert(0, prefix)
 
             key = '-'.join(parts)
-
             if key:
                 v = redis.get(key)
                 if v:
-                    print(v)
                     return pickle.loads(v)
 
             v = func(*args, **kwargs)
